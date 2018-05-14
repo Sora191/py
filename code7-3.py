ca4309eb40143fffcdb5667989091764b893a37a
191 <39262341+Sora191@users.noreply.github.com>
2018-05-15 06:08:50 +0900

    Create code7-3.py

---
+++ code7-3.py
@@ -0,0 +1,26 @@
+#code7-3.py
+
+def getGrades(fname):
+    try:
+        gradesFile = open(fname, 'r')
+    except IOError:
+        raise ValueError('getGrades could not open ' + fname)
+    grades = []
+    for line in gradesFile:
+        try:
+            grades.append(float(line))
+        except:
+            raise ValueError('Unable to convert line to float')
+    return grades
+
+try:
+    grades = getGrades('quiz1grades.txt')
+    grades.sort()
+    median = grades[len(grades)//2]
+    print 'Median grade is', median
+except ValueError, errorMsg:
+    print 'Whoops.',errorMsg
+
+
+
+Whoops. getGrades could not open quiz1grades.txt
