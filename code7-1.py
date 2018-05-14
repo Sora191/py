907ac506b435a95e8a7e872f6be8568177f0481e
191 <39262341+Sora191@users.noreply.github.com>
2018-05-15 06:15:27 +0900

    Update code7-1.py

--- code7-1.py
+++ code7-1.py
@@ -1,6 +1,8 @@
 #code7-1.py
 
 def getRatios(vect1, vect2):
+    """vect1とvect2を同じ長さのリストとする
+       vect1[i]とvect2[i]を意味する値からなるリストを返す"""
     ratios = []
     for index in range(len(vect1)):
         try:
@@ -10,8 +12,6 @@ def getRatios(vect1, vect2):
         except:
             raise ValueError('getRatios called with bad arguments')
     return ratios
-
-
 
 try:
     print getRatios([1.0,2.0,7.0,6.0], [1.0,2.0,0.0,3.0])
