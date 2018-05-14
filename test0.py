13d3a26b2b7c6bead176e2a11b0dcc15e46c885f
191 <39262341+Sora191@users.noreply.github.com>
2018-05-15 05:12:54 +0900

    Create test0.py

---
+++ test0.py
@@ -0,0 +1,10 @@
+#test0.py
+
+test = [1,2,3]
+test[3]
+
+
+Traceback (most recent call last):
+  File "D:\pleiades\workspace\rinkou\src\test.py", line 4, in <module>
+    test[3]
+IndexError: list index out of range
