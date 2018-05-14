732b05ae1db38d8fa68132ea0de0fd5780f7071c
191 <39262341+Sora191@users.noreply.github.com>
2018-05-15 06:21:09 +0900

    Update test0.py

--- test0.py
+++ test0.py
@@ -5,6 +5,6 @@ test[3]
 
 
 Traceback (most recent call last):
-  File "D:\pleiades\workspace\rinkou\src\test.py", line 4, in <module>
+  File "D:\pleiades\workspace\test\src\test.py", line 4, in <module>
     test[3]
 IndexError: list index out of range
